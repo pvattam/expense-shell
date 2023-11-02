eb606b6aaf185898efe40052cb48096ff205b440
Pushpak Vattam <vattampushpak243@gmail.com>
2023-11-02 21:05:58 +0530

    Making expense-shell frontend changes

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,7 @@ echo -e "\e[36m Installing Nginx \e[0m"
 dnf install nginx -y
 
 echo -e "\e[36m Copy Expense config file \e[0m"
-cp.expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf
 
 echo -e "\e[36m Clean Default Nginx content \e[0m"
 rm -rf /usr/share/nginx/html/*
